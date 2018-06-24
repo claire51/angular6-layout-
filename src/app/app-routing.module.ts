d9a342abd56a0b7a343367698634e88fef0f59db
kevol <kelvinmuiruri51@gmail.com>
2018-06-25 02:00:51 +0300

    lazy loadingfix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
 import { MyTableComponent } from './my-table/my-table.component';
 
+
 const routes: Routes = [
 
   { path: 'dashboard', component: MyDashboardComponent },
-  { path: 'datatable', component: MyTableComponent }
+  { path: 'datatable', component: MyTableComponent },
+  {
+    loadChildren: 'app/profile/profile.module#ProfileModule', path: 'profile'
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
