3acb66b913658009ccd286f9946c1dd03331b703
richard <richard.obiye@compulynx.com>
2018-07-02 17:07:40 +0300

    login fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
+import { LoginComponent } from './login/login.component';
 import { MyTableComponent } from './my-table/my-table.component';
 
 
 const routes: Routes = [
 
   { path: 'dashboard', component: MyDashboardComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'datatable', component: MyTableComponent },
   { path: 'profile',
     loadChildren: '../app/profile/profile.module#ProfileModule'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AuthGuardGuard } from './guard/auth-guard.guard';
 import { AuthService } from './auth.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyNavComponent } from './my-nav/my-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatSelectModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatSelectModule, MatIconModule, MatListModule,  MatInputModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule } from '@angular/material';
 import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
 import { MyTableComponent } from './my-table/my-table.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyNavComponent,
     MyDashboardComponent,
-    MyTableComponent
+    MyTableComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     LayoutModule,
     MatToolbarModule,
@@ -34,6 +38,7 @@ import { environment } from '../environments/environment';
     MatGridListModule,
     MatCardModule,
     MatMenuModule,
+    MatInputModule,
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
