a32830a0368ba3c4e59f372af80cfbf59ebaeeab
richard <richard.obiye@compulynx.com>
2018-07-07 11:25:17 +0300

    authentication & aall that

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AuthGuardGuard } from './guard/auth-guard.guard';
 import { AuthService } from './auth.service';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule , FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatDialogModule, MatButtonModule, MatSidenavModule, MatSelectModule, MatIconModule,
-MatListModule, MatInputModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+MatListModule, MatInputModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule } from '@angular/material';
 import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
 import { MyTableComponent } from './my-table/my-table.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -24,6 +24,7 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
+   FormsModule ,
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
