b1c573caf74bc073a3937f66f4f98d64e14d0e68
kevol <kelvinmuiruri51@gmail.com>
2018-07-21 20:49:00 +0300

    layout fix

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../auth.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+  form: FormGroup;
+  private formSubmitAttempt: boolean;
+  constructor(    private fb: FormBuilder,
+                  private authService: AuthService) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      firstname: ['', Validators.required],
+      middlename: ['', Validators.required],
+      lastname: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: ['', Validators.required],
+      idnumber: ['', Validators.required],
+      passowrd: ['', Validators.required]
+    });
+  }
+  isFieldInvalid(field: string) {
+    return (
+      (!this.form.get(field).valid && this.form.get(field).touched) ||
+      (this.form.get(field).untouched && this.formSubmitAttempt)
+    );
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.authService.login(this.form.value);
+    }
+    this.formSubmitAttempt = true;
+  }
+}
