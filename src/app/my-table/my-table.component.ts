59a4bf6e3e10278481020cdcec11d082dd4e5161
richard <richard.obiye@compulynx.com>
2018-08-25 13:24:55 +0300

    editing ..

--- src/app/my-table/my-table.component.ts
+++ src/app/my-table/my-table.component.ts
@@ -1,29 +1,51 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator, MatSort } from '@angular/material';
-import { MyTableDataSource } from './my-table-datasource';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {UserdataService} from '../localService/userdata';
 import {AuthService} from '../auth.service';
-import {User} from '../model/User';
 
 @Component({
   selector: 'my-table',
   templateUrl: './my-table.component.html',
   styleUrls: ['./my-table.component.css']
 })
-export class MyTableComponent implements OnInit {
-  data: Array<User> =  new Array<User>();
+export class MyTableComponent implements  AfterViewInit {
+  // data: Array<User>;
   constructor(  public userdata: UserdataService , public authservice: AuthService ) {
   }
-  @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-  dataSource: MyTableDataSource;
-
+  dataSource: MatTableDataSource<any>;
+  // @ViewChild(MatPaginator) paginator: MatPaginator;
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'first_name', 'middle_name', 'phone_number', 'email'];
+  displayedColumns = ['id', 'first_name', 'middle_name', 'phone_number', 'email', 'edit'];
 
 
-  ngOnInit() {
-    this.dataSource = new MyTableDataSource(this.paginator, this.sort,
-      this.userdata , this.authservice);
+  // ngOnInit() {
+    // this.dataSource = new MyTableDataSource(this.paginator, this.sort,
+    //   this.userdata , this.authservice);
+  // }
+
+  ngAfterViewInit() {
+    this.userdata.getdata().subscribe((data) => {
+      // this.data = newHeroWithId;
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.sort = this.sort;
+    }, (response: Response) => {
+      if (response.status <= 500) {
+        this.authservice.showSnackBar(' could not load users');
+      }
+    });
+  }
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
+  trackByUid(index, User1) {
+    return User1.id;
+  }
+
+  openDialog(data): void {
+
+  console.log(data);
   }
 }
