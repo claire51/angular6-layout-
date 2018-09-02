f954337646d958a5092b48a06eab76520c307d7a
kevol <kelvinmuiruri51@gmail.com>
2018-09-02 23:59:24 +0300

    transaction modify

--- src/app/Component/edittrade/edittrade.component.ts
+++ src/app/Component/edittrade/edittrade.component.ts
@@ -20,6 +20,8 @@ export class EdittradeComponent implements OnInit {
 
   firstFormGroupA: FormGroup;
   secondFormGroupB: FormGroup;
+  status: boolean;
+  private formSubmitAttempt: boolean;
 
   transaction: Transactions = new Transactions();
   item: Item = new Item();
@@ -28,36 +30,54 @@ export class EdittradeComponent implements OnInit {
 
   ngOnInit() {
     this.transaction = this.auth.transactionshelper;
-    this.item = this.transaction.items[0];
+    // this.item = this.transaction.items[0];
     this.firstFormGroupA = this._formBuilder.group({
       id: [this.transaction.id, Validators.required],
       period: [this.transaction.period, Validators.required],
       invoice_amount: [this.transaction.invoice_amount ],
       inspection_period: [this.transaction.inspection_period],
-      agent_fee_value: [this.transaction.agent_fee_value],
+      agent_fee_value: [10],
       agent_fee_type_id: [this.transaction.agent_fee_type_id],
       fee_allocation_id: [this.transaction.fee_allocation_id],
       classification_id: [ this.transaction.classification_id],
     });
     this.secondFormGroupB = this._formBuilder.group({
-      id: [this.item.id],
-      name: [this.item.name],
-      description: [this.item.description],
-      quantity: [this.item.quantity],
-      unit_of_measures_id: [this.item.unit_of_measures_id]
+      id: [1],
+      name: ['ddd'],
+      description: ['dddddddddddd'],
+      quantity: [3],
+      unit_of_measures_id: [1]
+      // id: [this.item.id],
+      // name: [this.item.name],
+      // description: [this.item.description],
+      // quantity: [this.item.quantity],
+      // unit_of_measures_id: [this.item.unit_of_measures_id]
     });
   }
-
+  isFieldInvalid(field: string) {
+    return (
+      (!this.firstFormGroupA.get(field).valid && this.firstFormGroupA.get(field).touched) ||
+      (this.firstFormGroupA.get(field).untouched && this.formSubmitAttempt)
+    );
+  }
+  isFieldInvalidB(field: string) {
+    return (
+      (!this.secondFormGroupB.get(field).valid && this.secondFormGroupB.get(field).touched) ||
+      (this.secondFormGroupB.get(field).untouched && this.secondFormGroupB)
+    );
+  }
   onSubmit() {
     if (this.firstFormGroupA.valid) {
       this.submittradevalueedit(this.firstFormGroupA.value);
     }
+    this.formSubmitAttempt = true;
   }
 
   onSubmitform() {
     if (this.secondFormGroupB.valid) {
       this.submittradeitemedit(this.secondFormGroupB.value);
     }
+    this.formSubmitAttempt = true;
   }
 
   submittradevalueedit(tradevaluedit: Tradevalueedit) {
